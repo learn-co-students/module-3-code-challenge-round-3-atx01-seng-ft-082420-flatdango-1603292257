a2d0a838a022aade98192d5bc7c6bd3a15263546
siegelverse <msiegel37@gmail.com>
2020-10-21 11:51:19 -0500

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-const url = "http://localhost:3000/films"
+const url = "http://localhost:3000/films/1"
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded")
+    fetch(url)
+    .then(res => res.json())
+    .then(movie => {
+        renderMovie(movie)
+    })
+})
+
+function renderMovie(movie) {
+    // console.log("hi")
+
+    const poster = document.getElementById("poster")
+    poster.src = movie.poster
+
+    const title = document.getElementById("title")
+    title.innerText = movie.title
+
+    const runtime = document.getElementById("runtime")
+    runtime.innerText = `${movie.runtime} minutes`
+
+    const plot = document.getElementById("film-info")
+    plot.innerText = movie.description
+
+    const showtime = document.getElementById("showtime")
+    showtime.innerText = movie.showtime
+
+    const ticket = document.getElementById("ticket-num")
+    let capacity = movie.capacity 
+    let ticketsSold = movie.tickets_sold
+    // console.log(capacity)
+    ticket.innerText = capacity - ticketsSold
+
+    const buyTixBtn = document.querySelector(".ui.orange.button")
+    buyTixBtn.addEventListener("click", () => {
+        if(ticket.innerText > 0){
+        let newTixSold = ticketsSold + 1
+        ticket.innerText = capacity - newTixSold
+        fetch(`http://localhost:3000/films/${movie.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                tickets_sold: movie.tickets_sold ++
+            })
+        })
+        .then(res => res.json())
+        .then(obj => ticketsSold = obj.tickets_sold)
+        console.log(ticket.innerText)
+    } else {
+        buyTixBtn.innerText = "Sold Out";
+    }
+    })
+    
+}
+
